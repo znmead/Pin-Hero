239570f873dc4a59a3523efad66c6b69b8f65767
Zach Mead <zachary.n.mead@gmail.com>
2021-02-23 11:52:48 -0600

    Added POST route, testing in Postman

--- server/routes/pin.router.js
+++ server/routes/pin.router.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 
 /**
  * GET route template
@@ -20,8 +23,19 @@ router.get('/', (req, res) => {
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
-  // POST route code here
+router.post('/', rejectUnauthenticated, (req, res) => {
+  // endpoint functionality
+  console.log('Adding new pin to DB');
+  const queryText = `INSERT INTO "pin" ("team", "league", "year", "image_url", "tradable", "user_id")
+  VALUES ($1, $2, $3, $4, $5, $6 ${req.user.id});`;
+
+  pool.query(queryText, [req.body.itemDescription, req.body.itemUrl]).then(() => {
+    console.log('Item added to shelf successfully');
+    res.sendStatus(201);
+  }).catch(err => {
+    console.log('Error in post', error);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/pin', pinRouter )
+app.use('/api/pin', pinRouter);
 
 // Serve static files
 app.use(express.static('build'));
