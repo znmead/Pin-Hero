7ad3d00a09b4687b5add59cb1dd65dd858e3d472
Zach Mead <zachary.n.mead@gmail.com>
2021-02-23 11:09:02 -0600

    GET route working

--- database.sql
+++ database.sql
@@ -8,3 +8,107 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "pin" (
+	"id" SERIAL PRIMARY KEY,
+	"team" VARCHAR (80),
+	"league" VARCHAR (80),
+	"year" INTEGER,
+	"image_url" VARCHAR (2083),
+	"tradeable" BOOLEAN,
+	"user_id" INTEGER REFERENCES "user"
+);
+
+-- NOTES / EXAMPLES
+INSERT INTO "songs" ("id", "rank", "artist", "track", "published")
+VALUES(1, 357, 'Doin Time', 'Sublime', '1-1-1996');
+
+INSERT INTO "songs" ("id", "rank", "artist", "track", "published")
+VALUES(2, 187, 'Dont Push', 'Sublime', '1-1-1996');
+
+/* clear a table */ 
+DROP TABLE "songs";
+
+/* selecting everything from a table */
+SELECT * FROM "songs";
+
+/* selecting specific columns */
+SELECT "track", "artist" FROM "songs";
+
+/* limiting results */ 
+SELECT * FROM "songs" LIMIT 10;
+
+/* specific data */ 
+SELECT * FROM "songs"
+WHERE "id" = 1;
+
+/* specific multiple data */ 
+SELECT * FROM "songs"
+WHERE "id" IN  (1, 2, 3);
+
+
+/* results that match... % match all */
+SELECT "track", "artist" FROM "songs"
+WHERE "artist" LIKE '%elvis%' LIMIT 10;
+
+/* results section */ 
+SELECT * FROM "songs"
+WHERE "id" BETWEEN 18 AND 27; 
+
+SELECT "track", "artist", "published" FROM "songs"
+WHERE "rank" <385 LIMIT 10; 
+
+/* ORDER BY column name ASC or DESC */
+SELECT "track", "artist", "published" FROM "songs"
+WHERE  "published" > '1/1/1980'
+ORDER BY "published" ASC LIMIT 20;
+
+
+/* WHERE conditions can be joined with AND, OR, & NOT */ 
+SELECT * FROM "songs"
+WHERE ("artist" LIKE '%a%' AND "published" > '1/1/2000')
+OR "id" > 400;
+
+/* last 20 years */ 
+SELECT * FROM "songs" WHERE "published" > NOW() - INTERVAL '20 years';
+
+SELECT * FROM "songs"; 
+WHERE "id" = 1;
+
+
+UPDATE 
+
+UPDATE "songs" SET "artist" = 'Chris Black'
+WHERE "id" = 1;
+
+/*single selection */ 
+SELECT * FROM "songs" WHERE "artist" LIKE 'Kanye West';
+
+/* delete selection */ 
+
+DELETE FROM "songs" WHERE "artist" LIKE 'Kanye West';
+
+/* group by */ 
+SELECT column1, function_name(column2)
+FROM table_name
+WHERE condition
+GROUP BY column1, column2
+ORDER BY column1, column2;
+
+/* count by */ 
+SELECT count(*) FROM "songs"
+
+/* upvote/downvote */
+
+UPDATE "songs" SET "rank" = "rank" +1 WHERE "id"==33; 
+
+
+/* select hobbies for a specific user person id 3 */
+
+SELECT * 
+FROM "person" 
+JOIN "person_hobby" ON = "person_id"
+JOIN "hobby" ON = "hobby_id"
+WHERE "person_hobby"."person_id" = 3; AVG
+
+
--- server/routes/pin.router.js
+++ server/routes/pin.router.js
@@ -6,8 +6,16 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-  // GET route code here
+  let query = 'SELECT * FROM "pin";'
+  pool
+    .query(query).then(
+      (result) => { res.send(result.rows) })
+    .catch(error => {
+      console.log("Couldn't get pins from DB", error);
+      res.sendStatus(500);
+    })
 });
+
 
 /**
  * POST route template
