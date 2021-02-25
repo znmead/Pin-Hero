6ac7fc5210988428cf70fd207ed4e3a6303b53bc
Zach Mead <zachary.n.mead@gmail.com>
2021-02-24 22:29:23 -0600

    Added 3 sagas

---
+++ src/redux/sagas/pin.saga.js
@@ -0,0 +1,57 @@
+import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+// GET all pins to hockey_pins db
+function* fetchPins(action) {
+    try {
+        if (action.payload) {
+            console.log('shipping', action.payload)
+            const pinResponse = yield axios.get(`/api/pin/${action.payload}`);
+        }
+        else {
+            const pinResponse = yield axios.get('/api/pin');
+        }
+        yield put({ type: 'SET_PIN', payload: pinResponse.data});
+    } catch (error) {
+        console.log('Error getting pins', error);
+    }
+};
+
+// POST new pin to hockey_pins db
+function* addItem(action) {
+    try {
+        yield axios.post('/api/pin', action.payload);
+        yield put({ type: 'FETCH_PIN' })
+    } catch (error) {
+        console.log('Error adding pin', error);
+    }
+};
+
+// TODO: function* editPin(action)
+
+// DELETE pin from hockey_pins db
+function deletePin(action) {
+    try {
+        yield axios.delete(`/api/pin/${action.payload}`);
+        yield put({ type: 'FETCH_ITEM' });
+    } catch (error) {
+        console.log('Error deleting pin', error);
+    }
+};
+
+
+
+
+
+
+
+
+
+function* pinSaga() {
+    yield takeLatest('FETCH_PIN', fetchPins);
+    yield takeLatest('ADD_PIN', addPin);
+    yield takeLatest('DELETE_PIN', deletePin);
+}
+
+export default pinSaga;
+
