02a7ca35be7a5f344f93576af9cc8eccc0201ec9
Zach Mead <zachary.n.mead@gmail.com>
2021-02-25 22:50:03 -0600

    Working User Page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import useReduxStore from "../../hooks/useReduxStore";
 import { useDispatch, useSelector } from 'react-redux';
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import swal from 'sweetalert';
 
-
-
 function UserPage() {
   let [addPinToggle, setPinToggle] = useState(false);
   const user = useSelector((store) => store.user);
   const pins = useSelector((store) => store.pins);
+  const store = useReduxStore();
+
   const dispatch = useDispatch();
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch({ type: 'ADD_PIN', payload: store.addPin });
     setPinToggle(false);
   };
-  
-  const deletePin = (id) => {
+
+  const handleDelete = (id) => {
     dispatch({ type: 'DELETE_PIN', payload: id })
   };
 
+  const handleTeamChange = (e) => {
+    dispatch({ type:'SET_USER_TEAM', payload: e.target.value });
+  };
 
-  
-  
-  
-  
+  const handleLeagueChange = (e) => {
+    dispatch({ type: 'SET_USER_LEAGUE', payload: e.target.value });
+  };
 
   useEffect(() => {
     dispatch({ type: 'FETCH_PIN' });
   }, []);
 
-  console.log('user, pins',user, pins);
+  console.log('user, pins', user, pins);
 
   return (
     <div className="container">
@@ -43,44 +46,46 @@ function UserPage() {
       <p>First Name: {user.first_name}</p>
       <p>Last Name: {user.last_name}</p>
       <p>Team: {user.team}</p>
-      <p>League: {user.league}</p> 
+      <p>League: {user.league}</p>
       <p>Jersey Number: {user.player_number}</p>
       <p>Your pins are: </p>
 
       <table>
-                <tbody>
-                    <tr>
-                        <td>Pins</td>
-                        <td>{pins.id}</td>
-                    </tr>
-                    <tr>
-                        <td>Image</td>
-                        <td>{pins.team}</td>
-                        <td><img src={pins.image_url} alt={pins.team} /></td>
-                    </tr>
-                    <tr>
-                        <td>Year</td>
-                        <td>{pins.year}</td>
-                        <td>League</td>
-                        <td>{pins.league}</td>
-                    </tr>
-                    <tr>
-                        <td>Tradeable?</td>
-                        <td>{pins.tradeable.toString()}</td>
-                    </tr>
-                </tbody>
-            </table>
+        <tbody>
+          <tr>
+            <td>Pins:</td>
+            <td>{pins.id}</td>
+          </tr>
+          <tr>
+            <td>Image:</td>
+            <td>{pins.team}</td>
+            <td><img src={pins.image_url} alt={pins.team} /></td>
+          </tr>
+          <tr>
+            <td>Year:</td>
+            <td>{pins.year}</td>
+          </tr>
+          <tr>
+            <td>League:</td>
+            <td>{pins.league}</td>
+          </tr>
+          <tr>
+            <td>Tradeable?</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
       <div className="pins">
         <ul>
           {pins.map(pin => {
             return (
-              <li key={pin.id}> 
-              {pin.id} {pin.year} {pin.league} {pin.team} {pin.tradeable.toString()} {pin.user_id}
+              <li key={pin.id}>
+                {pin.id} {pin.year} {pin.league} {pin.team} {pin.tradeable.toString()} {pin.user_id}
               &nbsp;
-              <button type="delete" 
-              value={pin.id} 
-              onClick={() => deletePin()}>Delete</button></li>
-              )
+                <button type="delete"
+                  value={pin.id}
+                  onClick={() => deletePin()}>Delete</button></li>
+            )
           })}
         </ul>
       </div>
