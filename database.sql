7ede4e9a14e228883245d3fb4447eea12ffece01
Zach Mead <zachary.n.mead@gmail.com>
2021-03-07 12:11:03 -0600

    SQL

--- database.sql
+++ database.sql
@@ -49,6 +49,57 @@ VALUES ('A Black', 'Squirt', 2021, 'https://hockypin.com', false, 1),
 ('A White', 'Squirt', 2021, 'https://hockypin.com', false, 1),
 ('A Pink', 'Squirt', 2021, 'https://hockypin.com', false, 1);
 
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "first_name" VARCHAR (80),
+    "last_name" VARCHAR (80),
+    "player_number" INTEGER,
+    "team" VARCHAR (255),
+    "league" VARCHAR (255)
+);
+
+UPDATE "user"
+SET "first_name"='Dwight', "last_name"='Shrute', "player_number"=23, 
+"team"='A Bears', "league"='Galactica';
+
+
+CREATE TABLE "pin" (
+	"id" SERIAL PRIMARY KEY,
+	"team" VARCHAR (80),
+	"league" VARCHAR (80),
+	"year" VARCHAR (4),
+	"image_url" VARCHAR (2083),
+	"tradeable" BOOLEAN DEFAULT FALSE,
+	"user_id" INTEGER REFERENCES "user"
+);
+
+UPDATE "pin" SET "tradeable" = CASE WHEN "tradeable"= "true" THEN SET "false" WHEN "tradeable" = "false" THEN SET "true" END SELECT * FROM "pin";
+
+
+
+DROP TABLE "pin"; 
+
+SELECT * FROM "user";
+
+INSERT INTO "user"
+VALUES
+(1
+
+SELECT * FROM "pin"; 
+
+UPDATE "pin" SET "tradeable"="true" WHERE "user_id"=$1
+
+
+INSERT INTO "pin" ("team", "league", "year", "image_url", "tradeable", "user_id")
+VALUES ('Goldrush', 'Squirt', 2021, 'images/2010-California-Goldrush.jpeg', false, 1),
+('Titans', 'Squirt', 2019, 'images/2019-Grand-Forks-Titans.jpeg', false, 1),
+('Trojans', 'Squirt', 2020, 'images/2020-New-Prague-Trojans.jpeg', false, 1),
+('Warriors', 'Squirt', 2021, 'images/Brainerd-Warriors.jpeg', false, 1),
+('Odancs', 'Squirt', 2021, 'images/Cando-Odnacs.jpeg', false, 1),
+('Jr Musketeers', 'Squirt', 2021, 'images/South-Carolina-Jr-Musketeers.jpeg', false, 1);
+
 
 
 
