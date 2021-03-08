9fe640b3d15d555a5c000a41d2c1d029134d2353
Zach Mead <zachary.n.mead@gmail.com>
2021-03-08 06:39:14 -0600

    About page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,18 +1,61 @@
 import React from 'react';
 
-// This is one of our simplest components
-// It doesn't have local state,
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is'
+import {
+  Button,
+  ButtonGroup,
+  Divider,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Heading,
+  HStack,
+  Input,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  MdCheckCircle,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Progress,
+  Radio,
+  RadioGroup,
+  Select,
+  Stack,
+  Switch,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Text,
+  useToast
+
+} from "@chakra-ui/react"
 
 function AboutPage() {
   return (
     <div className="container">
       <div>
       <p>
-            This app was made with the following technologies: <br></br>
+            <Heading as="h1" size="xl">This app was made with the following technologies</Heading> <br></br>
 
-            <br></br>
+            
             
             PERN Stack: <br></br>
             Postgresql - Backend database <br></br>
@@ -22,13 +65,13 @@ function AboutPage() {
 
             <br></br>
 
-            Some assistive technologies used: <br></br>
+            <Heading as="h1" size="xl">Assistive technologies</Heading><br></br>
 
             FileStack.js - image and files import handler <br></br>
             Chakra UI - Component library to make React beautiful <br></br>
 
             </p>
-            <hr></hr>
+            {/* <hr></hr> */}
             <br></br>
 
             
@@ -36,32 +79,21 @@ function AboutPage() {
           <br></br>
 
           <p>
-            Zen Of Javascript by Clayton Gulick
+          <Heading as="h1" size="xl">Future features</Heading>
 
             <hr></hr>
             <br></br>
 
-            Build functionality, not architecture.<br></br>
+            NHL api integration for news/scores<br></br>
             Build small things that work<br></br>
             and glue them together to make big things.<br></br>
             Clear is better than clever,<br></br>
-            but graceful beats both.<br></br>
-            Comments are good, even if you're Dutch.<br></br>
-            Asychronous is better than synchronous
-            except for when it isn't
-            but your code should work on both.<br></br>
-            Convention is better than definition,
-            if you have good docs.<br></br>
-            Keep it loose, but build good tests.<br></br>
+            
           </p>
 
           <br></br>
-          <hr></hr>
+          {/* <hr></hr> */}
           <br></br>
-
-          <p>
-            "I am faster than 80% of all snakes." - Dwight Shrute
-          </p>
 
       </div>
     </div>
